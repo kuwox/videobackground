afbfafb040ae5e18c9c28d9edeabfef42d6e2df4
Juan Miguel Spolzino Mendoza <juan.spolisino@gmail.com>
2024-07-16 15:25:22 -0400

    Compitabilidad iOS

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,14 +1,32 @@
+import { useEffect } from "react";
+
 export default function Hero() {
-    return (
-      <>
-        <video className="video-background" autoPlay muted loop playsInline>
-          <source src="/video.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <div className="content">
-          <h1>Bienvenido a mi App</h1>
-          <p>Este es un ejemplo de video de fondo.</p>
-        </div>
-      </>
-    );
-  }
+  useEffect(() => {
+    const video = document.getElementById("background-video");
+    // Intento de reproducir el video en iOS
+    if (video) {
+      video.play().catch((error) => {
+        console.log("Reproducción automática no permitida: ", error);
+      });
+    }
+  }, []);
+  return (
+    <>
+      <video
+        id="background-video"
+        className="video-background"
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source src="/video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="content">
+        <h1>Bienvenido a mi App</h1>
+        <p>Este es un ejemplo de video de fondo.</p>
+      </div>
+    </>
+  );
+}
